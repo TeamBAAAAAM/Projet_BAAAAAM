faa356c38f2866e20dd34d0c061269abbfcc0ce1
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-02-05 09:18:13 +0100

    Index + Dépôt
    Supression des boutons de la page d'accueil et ajout des nouveaux boutons dans le formulaire

--- frontOffice/script.js
+++ frontOffice/script.js
@@ -1,15 +1,4 @@
-var pj = ["salarie", "interim", "cesu", "pole-emploi",
-		  "pole-emploiC", "intermit", "independant", "art-aut"];
-
-//Au lancement de la page
-$(document).ready(function(){
-	$("#form_panel").hide(); //Le formulaire est masqué
-	
-	//Désélection de tous les boutons
-	for(i = 0; i < pj.length ; i++) {
-		$("#" + pj[i]).addClass("unselected");
-	}
-});
+var pj = ["salarie", "interim", "cesu", "pole-emploi", "pole-emploiC", "intermit", "independant", "art-aut"];
 
 //Scroll vers le div d'identifiants "id"
 function goToByScroll(id, duration) {
@@ -51,48 +40,55 @@ function isUnselected(selector) {
 //Cache tous les justificatifs
 function hideAllPJ() {
 	for(i = 0; i < pj.length ; i++) {
-		$("#" + pj[i]).hide();
+		$("." + pj[i]).hide();
 	}
 }
 
-function majPJ(pj) {
-	if(isUnselected("#" + pj)){
+//Création d'une fonction événementielle déclencher du clique
+//sur l'un des bouton $("#" + pj[i])
+function click_function(event){
+	var currentPJ = event.data.arg1;
+
+	if(isUnselected("#" + currentPJ)){
 		showForm();
 		
 		hideAllPJ();
-		$("." + pj).show(1000);
+		$("." + currentPJ).show(1000);
 		$(".selected").toggleClass("unselected selected");
-		$("#" + pj).toggleClass("unselected selected");
+		$("#" + currentPJ).toggleClass("unselected selected");
 
-		if((pj == "interim")
-		|| (pj == "art-aut")
-		|| (pj == "independant")
-		|| (pj == "cesu")
-		|| (pj == "independant")
-		|| (pj == "independant")
+		if(	  (currentPJ == "interim")
+			| (currentPJ == "art-aut")
+			| (currentPJ == "independant")
+			| (currentPJ == "cesu")
+			| (currentPJ == "independant")
+			| (currentPJ == "independant")
 		){
 			//Changement de texte
 			$("#nb_BS").text("12");	//12 Bulletins de salaire
 			$("#seuil_BS").text("l'arrêt de travail"); //Autres cas que celui de Pôle Emploi
 		}
-		else if(pj == "pole-emploi"){
+		else if(currentPJ == "pole-emploi"){
 			//Changement de texte
 			$("#nb_BS").text("4");	//4 Bulletins de salaire
 			$("#max_BS").text("l'inscription à Pôle Emploi"); //Cas à Pôle Emploi
 		}
-		else if(pj == "pole-emploiC"){
+		else if(currentPJ == "pole-emploiC"){
 			//Changement de texte
 			$("#nb_BS").text("3");	//4 Bulletins de salaire
 			$("#max_BS").text("l'inscription à Pôle Emploi"); //Cas à Pôle Emploi
 		}
 	}
-}
+ }
 
 //Affichage des zones de dépot des PJ en fonction
 //de la catégorie choisie via le nom des classes
 $(document).ready(function(){
-	//Masquage du formulaire au chargement de la page
+	$("#form_panel").hide(); //Le formulaire est masqué
+	
 	for(i = 0 ; i < pj.length ; i++) {
-		$("#" + pj[i]).click(majPJ(pj[i]));
+		var currentPJ = pj[i];
+		$("#" + currentPJ).addClass("unselected"); //Désélection de tous les boutons
+		$("#" + currentPJ).click({arg1: currentPJ}, click_function);
 	}
 });
