5f0131487957e16d0109b4cffc0a2ecd96d73c8f
malaikateuhi <malaikateuhi@gmail.com>
2020-04-13 00:54:29 +0200

    Update traiter.php

--- backOffice/traiter.php
+++ backOffice/traiter.php
@@ -80,7 +80,7 @@
 						<ul class="nav navbar-nav" id="menu">
 							<li><a href="accueil.php"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
 							<li><a href="corbeille_generale.php"><span class="glyphicon glyphicon-list-alt"></span> Corbeille générale</a></li>
-							<li class="active"><a href="ma_corbeille.php"><span class="glyphicon glyphicon-folder-open"></span> Ma Corbeille</a></li>
+							<li><a href="ma_corbeille.php"><span class="glyphicon glyphicon-folder-open"></span> Ma Corbeille</a></li>
 						</ul>
 
 						<ul class="nav navbar-nav navbar-right dropdown">
