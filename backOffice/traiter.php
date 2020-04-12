1b8c4435daedd1a922cc44adf4556d18385fd1f4
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-04-13 00:59:07 +0200

    Update traiter.php

--- backOffice/traiter.php
+++ backOffice/traiter.php
@@ -55,49 +55,47 @@
 			});
 		</script>
 
-        <title>BAAAAAM - Réception des documents</title>
+        <title>PJPE - Réception des documents</title>
 	</head>
 	<body>
 		<nav class="navbar navbar-default header">
 			<div class="container">
 				<div class="navbar-header">
-					<h1>Back Office</h1>
+					<h1>PJPE</h1>
 				</div>
 			</div>
 		</nav>
 
-		<nav class="navbar navbar-inverse navbar-static-top">
-			<h4>
-				<div class="container">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar2">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>                        
-						</button>
-					</div>
-					<div class="collapse navbar-collapse" id="myNavbar2">
-						<ul class="nav navbar-nav" id="menu">
-							<li><a href="accueil.php"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
-							<li><a href="corbeille_generale.php"><span class="glyphicon glyphicon-list-alt"></span> Corbeille générale</a></li>
-							<li><a href="ma_corbeille.php"><span class="glyphicon glyphicon-folder-open"></span> Ma Corbeille</a></li>
-						</ul>
-
-						<ul class="nav navbar-nav navbar-right dropdown">
-							<li class="dropdown">
-								<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-								<?php echo("$prenomT $nomT "); ?><span class="glyphicon glyphicon-user"></span><span class="glyphicon glyphicon-menu-down"></span>
-								</a>
-								<ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-									<li role="presentation"><a role="menuitem" href="#">Profil</a></li>
-									<li role="presentation" class="divider"></li>
-									<li role="presentation"><a role="menuitem" href="#">Se déconnecter</a></li>
-								</ul>
-							</li>						
-						</ul>
-					</div>
+		<nav class="navbar navbar-inverse navbar-static-top police">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar2">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>                        
+					</button>
 				</div>
-			</h4>
+				<div class="collapse navbar-collapse" id="myNavbar2">
+					<ul class="nav navbar-nav" id="menu">
+						<li><a href="accueil.php"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
+						<li><a href="corbeille_generale.php"><span class="glyphicon glyphicon-list-alt"></span> Corbeille générale</a></li>
+						<li><a href="ma_corbeille.php"><span class="glyphicon glyphicon-folder-open"></span> Ma Corbeille</a></li>
+					</ul>
+
+					<ul class="nav navbar-nav navbar-right dropdown">
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+							<?php echo("$prenomT $nomT "); ?><span class="glyphicon glyphicon-user"></span><span class="glyphicon glyphicon-menu-down"></span>
+							</a>
+							<ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+								<li role="presentation"><a role="menuitem" href="#">Profil</a></li>
+								<li role="presentation" class="divider"></li>
+								<li role="presentation"><a role="menuitem" href="#">Se déconnecter</a></li>
+							</ul>
+						</li>						
+					</ul>
+				</div>
+			</div>
 		</nav>
 		
 		<div class="container">
