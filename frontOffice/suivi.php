91f8611131566f210f4624ee2376f4003d247954
Axel TOA <axeltoa.edusup@yahoo.fr>
2020-04-27 22:02:11 +0200

    Mise à jour du footer et de l'entête

--- frontOffice/suivi.php
+++ frontOffice/suivi.php
@@ -106,7 +106,7 @@
 		
 		<!-- SCRIPT JAVASCRIPT (JQUERY / BOOTSTRAP 3.4.1 / SCRIPT LOCAL) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 		<script src="script.js"></script>
 		<script>
         	<?php if ($post_ok) : ?>
@@ -437,9 +437,9 @@
 			</div>
 		</div>
 
-        <footer class="container-fluid text-center">
+		<footer class="container-fluid text-center">
 			<div class="row">
-				<div class="col-sm-4">
+				<div class="col-sm-3">
 					<div class="title_footer">Présentation</div>
 					<div class="row">
 						<p>Ajouter une description ici.</p>
@@ -448,27 +448,41 @@
 				<div id="links" class="col-sm-3">
 					<div class="title_footer">Liens utiles</div>
 					<div class="row">
-						<a class="col-sm-12" href="https://www.ameli.fr/haute-garonne" target="_blank">Site ameli - Haute Garonne</a>
-						<a class="col-sm-12"  href="https://assure.ameli.fr/PortailAS/appmanager/PortailAS/assure?_somtc=true" target="_blank">Mon compte ameli</a>
+						<div class="col-sm-12">
+							<a class="col-sm-12" href="https://www.ameli.fr/haute-garonne" target="_blank">
+								Site ameli - Haute Garonne
+							</a>
+						</div>
+						<div class="col-sm-12">
+							<a class="col-sm-12" href="https://assure.ameli.fr/PortailAS/appmanager/PortailAS/assure?_somtc=true" target="_blank">
+								Mon compte ameli
+                            </a>
+						</div>
+						<div class="col-sm-12">
+							<a class="col-sm-12" href="https://www.oups.gouv.fr/">
+								<img src="../img/logo_oups.svg" alt="Logo oups.gouv.fr">
+							</a>
+						</div>
 					</div>
 				</div>
-				<div class="col-sm-5">
+				<div class="col-sm-6">
 					<div class="title_footer">Contact</div>
-					<div class="row">
+					<div id="contact" class="row">
 						<iframe class="col-sm-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23113.985644999426!2d1.4384851395507818!3d43.601373400000014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aebc91ae4a5ba3%3A0x5d4ac376bccc8d50!2sCPAM%20de%20la%20Haute-Garonne!5e0!3m2!1sfr!2sfr!4v1580058891942!5m2!1sfr!2sfr" style="border:0;" allowfullscreen=""></iframe>	
 						<div class="col-sm-7 text-left">
-							<p>Caisse d'Assurance Maladie</p>
-							<p>Toulouse - Haute-Garonne (31)</p>
-							<p>3 Boulevard du Professeur Léopold Escande</p>
-							<p>31093 Toulouse</p>
-							<p>08 11 70 36 46</p>
-							<p>Ouvert du lundi au vendredi de 08h00 à 17h00</p>
-							<p>Fermé le samedi et le dimanche</p>
+							<div class="col-sm-12">Caisse Primaire d'Assurance Maladie</div>
+							<div class="col-sm-12">3, Boulevard du Professeur Léopold Escande</div>	
+							<div class="col-sm-12">Haute-Garonne (31) - 31093 Toulouse</div>													
+							<div class="col-sm-12">	
+								<img src="../img/num_tel.png" alt="36 46">
+							</div>																										
+							<div class="col-sm-12">Ouvert du lundi au vendredi de 08h00 à 17h00</div>														
+							<div class="col-sm-12">Fermé le samedi et le dimanche</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div id="juridique" class="row">
+			<div id="juridique" class="row">				
 				<div class="col-sm-3">
 					<a href="../documentation-juridique/cgu.html" target="_blank">Conditions Générales d'Utilisation</a>
 				</div>				
@@ -483,7 +497,7 @@
 				</div>
 			</div>
 			<div id="copyright" class="row">© 2020 Copyright - Tous droits réservés : Team BAAAAAM</div>
-		</footer>
+        </footer>
 		
     </body>
 </html>
