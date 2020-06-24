2953019afa0e9bd80efced27576ebf843a983f49
aissatou1702 <aissatou1702@yahoo.fr>
2020-06-24 21:53:11 +0200

    Gestion catégorie et mnémonique

---
+++ admin/enregistrer_categorie.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+    require_once("../fonctions.php");    
+    //Connexion à la BD
+    $link = connecterBD();
+
+if(isset($_POST["nomC"]) and isset($_POST["designationC"]) and isset($_POST["mnemonique"]) and isset($_POST["labelC"])){
+     
+     
+    //Enregistrement dans la table catégorie
+    $nomC = $_POST['nomC'];
+    $designationC = $_POST['designationC'];
+   
+    
+     $query = 'INSERT INTO categorie
+   (NomC, DesignationC, StatutC)
+VALUES
+   ("'.$nomC.'", "'.$designationC.'", "Actif");';
+    
+    mysqli_query($link, $query);
+    
+    //On va faire une requete pour recuperer l'id de la catégorie qui vient d'etre enregistré
+    $query_id_categorie = "SELECT CodeC FROM categorie ORDER BY CodeC DESC LIMIT 1 ";
+    $id_categorie = null;
+    
+    $result = mysqli_query($link, $query_id_categorie);
+    
+    if ($result != NULL){
+        $rows = mysqli_num_rows($result);
+        for ($i = 0; $i < $rows; $i++) {
+            $donnees = mysqli_fetch_array($result);
+        }
+        
+        $id_categorie = $donnees['CodeC'];
+    }
+
+    
+    //On recupère la liste (stocké dans un tableau array) des id des mnémoniques qui sont cochés plus leurs label
+   /* foreach ($_POST["mnemonique"] as $valeur) {
+        
+        echo $valeur."<br>"." ";
+        
+    }
+    
+    foreach ($_POST["label"] as $valeur) {
+        
+        echo $valeur."<br>";
+        
+   }
+    */
+    
+    //Alimente la table concerne avec les données correspondantes
+    
+    
+    
+    
+    
+    
+    //Redirection finale
+    header('Location: creation_categorie.php?msg=Success');
+    exit();
+    
+}
+?>
+
+
